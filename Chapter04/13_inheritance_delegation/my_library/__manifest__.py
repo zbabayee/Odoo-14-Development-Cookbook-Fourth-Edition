6d9242a830e32f6de8933a4a38e2b54d09ae28cc
zbabayee <96326004+zbabayee@users.noreply.github.com>
2022-08-20 08:27:32 +0430

    update manifest for adding library member views

--- Chapter04/13_inheritance_delegation/my_library/__manifest__.py
+++ Chapter04/13_inheritance_delegation/my_library/__manifest__.py
@@ -17,7 +17,8 @@ Description related to library.
         'security/groups.xml',
         'security/ir.model.access.csv',
         'views/library_book.xml',
-        'views/library_book_categ.xml'
+        'views/library_book_categ.xml',
+        'views/library_book_member.xml'
     ],
     # This demo data files will be loaded if db initialize with demo data (commented becaues file is not added in this example)
     # 'demo': [
